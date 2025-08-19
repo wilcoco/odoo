89c75fb2bbf5b20d5387174dc8ee2629cc565d88
wilcoco <myvboro@gmail.com>
2025-08-19 16:44:13 +0900

    feat: extract scanner into module 'barcode_scanner_widget' and depend from mrp_bom_scan_guard

---
+++ addons_custom/barcode_scanner_widget/__manifest__.py
@@ -0,0 +1,27 @@
+{
+    'name': "Barcode Scanner Widget",
+    'version': '18.0.0.1',
+    'depends': ['web'],
+    'author': "DevSanx",
+    'license': 'LGPL-3',
+    'category': "Tools",
+    'summary': 'Qrcode/Barcode Scanner',
+    'description': """
+        Scan Barcodes and Qrcodes using device camera
+    """,
+    'application': True,
+    'data': [],
+    'assets': {
+        'web.assets_backend': [
+            "barcode_scanner_widget/static/src/css/webcam_qrcode_scan_styles.css",
+            # Optional camera libraries: place files under static/src/lib/ and uncomment below lines.
+            # "barcode_scanner_widget/static/src/lib/html5-qrcode.min.js",
+            # "barcode_scanner_widget/static/src/lib/quagga.min.js",
+            "barcode_scanner_widget/static/src/js/barcode_scanner_widget.js",
+        ],
+        'web.assets_qweb': [
+            "barcode_scanner_widget/static/src/xml/webcam_qrcode_scan_template.xml",
+        ],
+    },
+    'installable': True,
+}
