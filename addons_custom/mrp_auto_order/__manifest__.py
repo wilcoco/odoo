b381137ba644bef10c54faac0b8f928b0a12f5d7
wilcoco <myvboro@gmail.com>
2025-08-28 07:33:21 +0900

    Revert "mrp_auto_order: batch UI/access + wizard improvements"
    This reverts commit a0c22b433d42c461f0d96c05c03c4717a8f022fc.

--- addons_custom/mrp_auto_order/__manifest__.py
+++ addons_custom/mrp_auto_order/__manifest__.py
@@ -1,13 +1,12 @@
 {
     'name': 'MRP Auto Order',
-    'version': '18.0.1.1.0',
+    'version': '18.0.1.0.1',
     'summary': 'Create and auto-confirm Manufacturing Orders from an order list',
     'category': 'Manufacturing',
     'depends': ['mrp'],
     'data': [
         'security/ir.model.access.csv',
         'views/mrp_auto_order_views.xml',
-        'views/mrp_auto_order_batch_views.xml',
     ],
     'license': 'LGPL-3',
     'author': 'DevSanx',
